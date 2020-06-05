795b3dd7a750133b86c4b4bcbe7918121b26189d
Amulet158 <minion.mumbaii@gmail.com>
2020-06-06 01:08:21 +0530

    Caching the mean of the vector

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<- NULL
+  set <- function(matrix){
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  get <- function(){
+    m
+  }
+  
+  setInverse <- function(inverse){
+    i <<- inverse
+  }
+  
+  getInverse <- function() {
+    i
+  }
+  
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
@@ -12,4 +30,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  if(!is.null(m)){
+    message("Getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
 }
